017e35e574e61aca91019b9c029766f0d7f87a8a
Igor Sakhno <ihar.sakhno@gmail.com>
2019-12-24 17:43:15 +0300

    Done

--- calendar.py
+++ calendar.py
@@ -10,15 +10,7 @@ while True:
     else:
         break
 
-# if (x < 1) and (x > 12):
-#     print("Введите номер месяца от 1 до 12")
-
-    # try:
-    #     (x < 1) and (x > 12)
-    # except tuple index out of range:
-    #     print("Введите номер месяца от 1 до 12")
-
 def logic_calendar(y):
     y = int(x) - 1
     return y
-print("Порядковый месяц " + str(x) + " это " + (year_rus[logic_calendar(x)]) + " по Аннглийски это " + (year_eng[logic_calendar(x)]))
+print("Порядковый месяц " + str(x) + " это " + (year_rus[logic_calendar(x)]) + " по Английски это " + (year_eng[logic_calendar(x)]))
